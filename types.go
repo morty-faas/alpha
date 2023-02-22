87fd6fac3a3f5534067d0b2e5c1a3181dfabbed4
thomasgouveia <gouveia.thomas@outlook.fr>
2023-02-22 13:03:50 +0100

    feat: add process metadata to response

--- types.go
+++ types.go
@@ -1,6 +1,16 @@
 package main
 
 type (
+	InstrumentedResponse struct {
+		Payload any              `json:"payload"`
+		Process *ProcessMetadata `json:"process_metadata"`
+	}
+
+	ProcessMetadata struct {
+		ExecutionTimeMs int64    `json:"execution_time_ms"`
+		Logs            []string `json:"logs"`
+	}
+
 	HealthResponse struct {
 		Status string `json:"status"`
 	}
